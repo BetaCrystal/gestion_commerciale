2bc0af3977271aa427e3a7a98b21576b27538c55
Grogdurut <antoine.rossi0102@gmail.com>
2025-11-14 15:59:15 +0100

    commit projet GesionCommerciale liste,ajout,modif,suppresion

---
+++ GestionCommerciale/ListeProduits.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace GestionCommerciale
+{
+    partial class ListeProduits
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dtgProduit = new System.Windows.Forms.DataGridView();
+            this.dataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new GestionCommerciale.DataSet1();
+            this.btnProduit = new System.Windows.Forms.Button();
+            this.colLibelle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPrix = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCategorie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modifier = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Supprimer = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProduit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgProduit
+            // 
+            this.dtgProduit.AutoGenerateColumns = false;
+            this.dtgProduit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgProduit.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colLibelle,
+            this.colPrix,
+            this.colCategorie,
+            this.Modifier,
+            this.Supprimer});
+            this.dtgProduit.DataSource = this.dataSet1BindingSource;
+            this.dtgProduit.Location = new System.Drawing.Point(33, 23);
+            this.dtgProduit.Name = "dtgProduit";
+            this.dtgProduit.RowHeadersWidth = 51;
+            this.dtgProduit.RowTemplate.Height = 24;
+            this.dtgProduit.Size = new System.Drawing.Size(755, 193);
+            this.dtgProduit.TabIndex = 0;
+            this.dtgProduit.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgProduit_CellContentClick);
+            // 
+            // dataSet1BindingSource
+            // 
+            this.dataSet1BindingSource.DataSource = this.dataSet1;
+            this.dataSet1BindingSource.Position = 0;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // btnProduit
+            // 
+            this.btnProduit.Location = new System.Drawing.Point(310, 301);
+            this.btnProduit.Name = "btnProduit";
+            this.btnProduit.Size = new System.Drawing.Size(182, 48);
+            this.btnProduit.TabIndex = 1;
+            this.btnProduit.Text = "Ajouter un Produit";
+            this.btnProduit.UseVisualStyleBackColor = true;
+            this.btnProduit.Click += new System.EventHandler(this.btnProduit_Click);
+            // 
+            // colLibelle
+            // 
+            this.colLibelle.DataPropertyName = "libelle_produit";
+            this.colLibelle.HeaderText = "Nom du produit";
+            this.colLibelle.MinimumWidth = 6;
+            this.colLibelle.Name = "colLibelle";
+            this.colLibelle.Width = 125;
+            // 
+            // colPrix
+            // 
+            this.colPrix.DataPropertyName = "prix_vente_ht_produit";
+            this.colPrix.HeaderText = "Prix hors taxe";
+            this.colPrix.MinimumWidth = 6;
+            this.colPrix.Name = "colPrix";
+            this.colPrix.Width = 125;
+            // 
+            // colCategorie
+            // 
+            this.colCategorie.DataPropertyName = "nom_categorie";
+            this.colCategorie.HeaderText = "Catégorie";
+            this.colCategorie.MinimumWidth = 6;
+            this.colCategorie.Name = "colCategorie";
+            this.colCategorie.Width = 125;
+            // 
+            // Modifier
+            // 
+            this.Modifier.HeaderText = "Modifier";
+            this.Modifier.MinimumWidth = 6;
+            this.Modifier.Name = "Modifier";
+            this.Modifier.Text = "modifier";
+            this.Modifier.UseColumnTextForButtonValue = true;
+            this.Modifier.Width = 125;
+            // 
+            // Supprimer
+            // 
+            this.Supprimer.HeaderText = "Supprimer";
+            this.Supprimer.MinimumWidth = 6;
+            this.Supprimer.Name = "Supprimer";
+            this.Supprimer.Text = "supprimer";
+            this.Supprimer.UseColumnTextForButtonValue = true;
+            this.Supprimer.Width = 125;
+            // 
+            // ListeProduits
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnProduit);
+            this.Controls.Add(this.dtgProduit);
+            this.Name = "ListeProduits";
+            this.Text = "Liste des produits";
+            this.Load += new System.EventHandler(this.FormListeProduits_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProduit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgProduit;
+        private System.Windows.Forms.BindingSource dataSet1BindingSource;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.Button btnProduit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLibelle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPrix;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCategorie;
+        private System.Windows.Forms.DataGridViewButtonColumn Modifier;
+        private System.Windows.Forms.DataGridViewButtonColumn Supprimer;
+    }
+}
---
+++ GestionCommercialeDAL/ProduitDAL.cs
@@ -0,0 +1,124 @@
+﻿using GestionCommercialeBO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCommercialeDAL
+{
+    public class ProduitDAL
+    {
+        private readonly string connectionString =
+            "Data Source=psl23-p16;Initial Catalog=Gestion_commerciale;Integrated Security=True;";
+
+        public List<Produit> GetProduits()
+        {
+            List<Produit> produits = new List<Produit>();
+
+            string query = @"
+                SELECT p.code_produit, p.libelle_produit, p.prix_vente_ht_produit, c.nom_categorie
+                FROM produit p
+                JOIN categorie c ON p.code_categorie = c.code_categorie";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    produits.Add(new Produit
+                    {
+                        code_produit = Convert.ToInt32(reader["code_produit"]),
+                        libelle_produit = Convert.ToString(reader["libelle_produit"]),
+                        prix_vente_ht_produit = Convert.ToDecimal(reader["prix_vente_ht_produit"]),
+                        nom_categorie = Convert.ToString(reader["nom_categorie"])
+                    });
+                }
+            }
+
+            return produits;
+        }
+
+        public void SupprimerProduit(int codeProduit)
+        {
+            string query = "DELETE FROM produit WHERE code_produit = @code";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@code", codeProduit);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertProduit(Produit p)
+        {
+            string query = @"INSERT INTO produit (libelle_produit, prix_vente_ht_produit, code_categorie) 
+                         VALUES (@libelle, @prix, @categorie)";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@libelle", p.libelle_produit);
+                cmd.Parameters.AddWithValue("@prix", p.prix_vente_ht_produit);
+                cmd.Parameters.AddWithValue("@categorie", p.code_categorie);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public Produit GetProduitById(int codeProduit)
+        {
+            Produit produit = null;
+            string query = "SELECT code_produit, libelle_produit, prix_vente_ht_produit, code_categorie FROM produit WHERE code_produit = @code";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@code", codeProduit);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        produit = new Produit
+                        {
+                            code_produit = Convert.ToInt32(reader["code_produit"]),
+                            libelle_produit = Convert.ToString(reader["libelle_produit"]),
+                            prix_vente_ht_produit = Convert.ToDecimal(reader["prix_vente_ht_produit"]),
+                            code_categorie = Convert.ToInt32(reader["code_categorie"])
+                        };
+                    }
+                }
+            }
+            return produit;
+        }
+
+        public void UpdateProduit(Produit p)
+        {
+            string query = @"UPDATE produit 
+                     SET libelle_produit = @libelle, 
+                         prix_vente_ht_produit = @prix, 
+                         code_categorie = @categorie
+                     WHERE code_produit = @code";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@libelle", p.libelle_produit);
+                cmd.Parameters.AddWithValue("@prix", p.prix_vente_ht_produit);
+                cmd.Parameters.AddWithValue("@categorie", p.code_categorie);
+                cmd.Parameters.AddWithValue("@code", p.code_produit);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
